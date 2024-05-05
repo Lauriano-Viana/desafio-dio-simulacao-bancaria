59eda0acfecae6fc5a792a12e15b4ccc19ee9c3b
Lariano <laurianoviana12@gmail.com>
2024-05-05 08:27:40 -0300

    desafio dio simulacao bancaria

---
+++ src/SimulacaoBancaria.java
@@ -0,0 +1,66 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+   public static void main(String[] args) {
+
+
+        try {
+            menu();
+        } catch (InputMismatchException e) {
+            System.out.println("Valor incorreto tente novamente");
+            menu();
+        }
+
+    }
+
+    static void menu(){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double saldo = 0.0;
+        int opcao = 3;
+        while (opcao !=0) {
+            double valor = 0.0;
+
+            System.out.println("Bem vindo ao nosso Banco \n" +
+                    "Por favor escolha uma das opções abaixo: \n" +
+                    "1) Depósito \n" +
+                    "2) Saques \n" +
+                    "3) Consultar saldo \n" +
+                    "0) Encerrar ");
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o valor do deposito");
+                    valor = scanner.nextDouble();
+                    saldo = saldo + valor;
+                    System.out.printf("Saldo atual: %.1f \n", saldo);
+                    break;
+
+                case 2:
+                    System.out.println("Digite o valor do saque");
+                    valor = scanner.nextDouble();
+                    if (saldo >= valor) {
+                        saldo = saldo - valor;
+                        System.out.printf("Saldo atual: %.1f \n", saldo);
+                    } else {
+                        System.out.println("Saldo Insuficiente.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.printf("Saldo atual: %.1f \n", saldo);
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+
+            }
+
+        }
+    }
+}
